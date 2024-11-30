7115cbe823274e22db444ae5d6dc618205b367c6
kimjuock <bihwaness404@gmail.com>
2024-11-30 20:09:03 +0900

    채팅1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'; // App.js를 임포트
+import reportWebVitals from './reportWebVitals'; // 웹 비탈리티 측정을 위한 코드
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// 애플리케이션을 'root'라는 id를 가진 div에 렌더링합니다.
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root') // 이 부분이 HTML에서 id가 'root'인 요소에 렌더링됩니다.
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 웹 비탈리티 측정을 원한다면 아래 코드를 활성화하세요
 reportWebVitals();
